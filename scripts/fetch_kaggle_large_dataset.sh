e86b6789d23899a2d61792df83d48986ef90d261
marcmartinezcamarena@gmail.com <mmartinez@autis.es>
2021-01-10 22:26:10 +0100

    opening pr138new3

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -11,9 +11,9 @@ ls "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedl
 FOLDER=(`ls ./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/`)
 echo ${FOLDER[14]}
 echo ${FOLDER[15]}
-VAR1= "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/"
-VAR2={FOLDER[14]}
-VAR3={FOLDER[15]}
+VAR1="./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/"
+VAR2=${FOLDER[14]}
+VAR3=${FOLDER[15]}
 VAR4=" "
 echo "$VAR1$VAR2$VAR4$VAR3"
 #mv -v "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherd's Purse/"* "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
